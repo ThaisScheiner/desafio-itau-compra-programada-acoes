22984039a99027372f793dbcbc86414283378944
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2026-03-06 17:51:57 -0300

    feat(cotacoes): configure cotacoes persistence

---
+++ src/services/CotacoesService/src/CotacoesService.Api/CotacoesService.Api/Infrastructure/Persistence/CotacoesDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.Persistence;
+using CotacoesService.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CotacoesService.Api.Infrastructure.Persistence;
+
+public sealed class CotacoesDbContext(DbContextOptions<CotacoesDbContext> options) : AppDbContextBase(options)
+{
+    public DbSet<Cotacao> Cotacoes => Set<Cotacao>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Cotacao>(b =>
+        {
+            b.ToTable("Cotacoes");
+            b.HasKey(x => x.Id);
+
+            b.Property(x => x.DataPregao).IsRequired();
+            b.Property(x => x.Ticker).HasMaxLength(10).IsRequired();
+
+            b.Property(x => x.PrecoAbertura).HasPrecision(18, 4).IsRequired();
+            b.Property(x => x.PrecoFechamento).HasPrecision(18, 4).IsRequired();
+            b.Property(x => x.PrecoMaximo).HasPrecision(18, 4).IsRequired();
+            b.Property(x => x.PrecoMinimo).HasPrecision(18, 4).IsRequired();
+
+            b.HasIndex(x => new { x.Ticker, x.DataPregao }).IsUnique();
+            b.HasIndex(x => x.DataPregao);
+        });
+    }
+}
