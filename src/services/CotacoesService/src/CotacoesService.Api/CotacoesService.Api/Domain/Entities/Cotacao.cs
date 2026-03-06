c092f83154cb8a7a454730254d7c9a97f35fd389
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2026-03-06 17:51:01 -0300

    feat(cotacoes): add cotacao domain entity

---
+++ src/services/CotacoesService/src/CotacoesService.Api/CotacoesService.Api/Domain/Entities/Cotacao.cs
@@ -0,0 +1,12 @@
+﻿namespace CotacoesService.Api.Domain.Entities;
+
+public sealed class Cotacao
+{
+    public long Id { get; set; }               
+    public DateTime DataPregao { get; set; }   
+    public string Ticker { get; set; } = "";   
+    public decimal PrecoAbertura { get; set; } 
+    public decimal PrecoFechamento { get; set; } 
+    public decimal PrecoMaximo { get; set; }   
+    public decimal PrecoMinimo { get; set; }   
+}
